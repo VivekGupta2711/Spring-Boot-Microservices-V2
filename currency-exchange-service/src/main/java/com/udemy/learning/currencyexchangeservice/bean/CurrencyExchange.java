1881753322ef36e0e3bba74943caa9b8b758bd63
Vivek Gupta <vivekgupta271186@gmailcom>
2023-12-11 12:19:35 +0000

    Onboarded JPA & H2 Database

--- currency-exchange-service/src/main/java/com/udemy/learning/currencyexchangeservice/bean/CurrencyExchange.java
+++ currency-exchange-service/src/main/java/com/udemy/learning/currencyexchangeservice/bean/CurrencyExchange.java
@@ -1,13 +1,28 @@
 package com.udemy.learning.currencyexchangeservice.bean;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Data
+@Entity
+@NoArgsConstructor(force = true)
+@RequiredArgsConstructor
 public class CurrencyExchange {
+
+    @Id
     private final Long id;
+    //from is an SQL identifier hence we cannot have a column name from
+    //Hence need to changed this column name
+    @Column(name = "currency_from")
     private final String from;
+    @Column(name = "currency_to")
     private final String to;
     private final BigDecimal conversionMultiple;
     private String environment;
